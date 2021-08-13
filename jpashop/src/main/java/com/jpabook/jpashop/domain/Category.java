1ca1e1c08e550feb2fa5807acf3056c97dae1089
sw-develop <psw00719@naver.com>
2021-08-13 12:10:34 +0900

    [ADD] Item과 ManyToMany - JoinTable 생성

---
+++ jpashop/src/main/java/com/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,30 @@
+package com.jpabook.jpashop.domain;
+
+import com.jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    private Category parent; //부모 카테고리
+
+    private List<Category> child = new ArrayList<>(); //자식 카테고리
+}
