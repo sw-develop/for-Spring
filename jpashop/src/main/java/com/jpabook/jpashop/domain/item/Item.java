6653693027158656e966c43d4d3011591e181535
sw-develop <psw00719@naver.com>
2021-08-13 12:11:48 +0900

    [ADD] Category와 ManyToMany

---
+++ jpashop/src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,30 @@
+package com.jpabook.jpashop.domain.item;
+
+import com.jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public abstract class Item { //구현체를 가지므로 추상클래스로 선언함
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+}
