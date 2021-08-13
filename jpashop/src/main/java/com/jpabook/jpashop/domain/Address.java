2e47c41687514a6ef0db70f7ad08b23070106795
sw-develop <psw00719@naver.com>
2021-08-13 11:06:10 +0900

    [ADD] 주소 내장 타입

---
+++ jpashop/src/main/java/com/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,14 @@
+package com.jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable //내장 타입
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
