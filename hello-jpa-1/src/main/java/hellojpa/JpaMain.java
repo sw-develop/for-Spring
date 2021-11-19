08a399e62a4290caee9b925abf64a860a4694b07
sw-develop <psw00719@naver.com>
2021-11-19 18:16:46 +0900

    Add: JPA 실습 프로젝트 생성 - build : Gradle - /META-INF/persistenct.xml : JPA 설정 추가 - batch_size 설정

---
+++ hello-jpa-1/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("changetestA");
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
