da24deabd32ffa78d96e940a58d8ce24b55a2ad2
sw-develop <psw00719@naver.com>
2022-03-02 01:05:15 +0900

    [jpashop] Add: 컬렉션 조회 최적화 - 컬렉션인 일대다 관계를 조회하고, 최적화 하는 방법 - V2: 엔티티를 DTO로 변환 - V3: 엔티티를 DTO로 변환, ToOne 관계 페치조인, batch_fetch_size 설정

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,94 @@
+package jpabook.jpashop.api;
+
+import static java.util.stream.Collectors.toList;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.respository.OrderRepository;
+import jpabook.jpashop.respository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+  private final OrderRepository orderRepository;
+
+  /* 주문 조회 V2: 엔티티를 DTO로 변환 */
+  @GetMapping("/api/v2/orders")
+  public Result ordersV2() {
+    List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+    List<OrderDto> collect = orders.stream()
+        .map(o -> new OrderDto(o))
+        .collect(toList());
+    return new Result(collect);
+  }
+
+  /* 주문 조회 V3.1: 엔티티를 DTO로 변환 후 페이징 고려
+  * - ToOne 관계만 우선 모두 페치 조인으로 최적화
+  * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+  * */
+  @GetMapping("/api/v3.1/orders")
+  public Result ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+      @RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+    List<Order> orders = orderRepository.findAllWithMemberDelivery(offset,limit);
+    List<OrderDto> collect = orders.stream()
+        .map(o -> new OrderDto(o))
+        .collect(toList());
+    return new Result(collect);
+  }
+
+  @Data
+  static class OrderDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate; //주문시간
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemDto> orderItems;
+
+    public OrderDto(Order order) {
+      orderId = order.getId();
+      name = order.getMember().getName();
+      orderDate = order.getOrderDate();
+      orderStatus = order.getStatus();
+      address = order.getDelivery().getAddress();
+      orderItems = order.getOrderItems().stream()
+          .map(orderItem -> new OrderItemDto(orderItem))
+          .collect(toList());
+    }
+  }
+
+  @Data
+  static class OrderItemDto {
+
+    private String itemName;//상품 명
+    private int orderPrice; //주문 가격
+    private int count; //주문 수량
+
+    public OrderItemDto(OrderItem orderItem) {
+      itemName = orderItem.getItem().getName();
+      orderPrice = orderItem.getOrderPrice();
+      count = orderItem.getCount();
+    }
+  }
+
+  /* Json Object 형태로 반환하기 위함 */
+  @Data
+  @AllArgsConstructor
+  static class Result<T> {
+    private T data;
+  }
+}
