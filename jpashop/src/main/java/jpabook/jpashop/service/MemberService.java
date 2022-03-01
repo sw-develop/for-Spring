0c59aea487c8afcd1840ffece511e02c1ae6d618
sw-develop <psw00719@naver.com>
2022-03-01 19:00:53 +0900

    [jpashop] Add: 회원 등록, 수정, 조회 API - DTO 클래스를 생성하여 요청과 응답에 반드시 사용한다.

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -40,4 +40,10 @@ public class MemberService {
     return memberRepository.findOne(memberId);
   }
 
+  /* 회원 이름 수정 */
+  @Transactional
+  public void update(Long id, String name) {
+    Member member = memberRepository.findOne(id);
+    member.setName(name);
+  }
 }
