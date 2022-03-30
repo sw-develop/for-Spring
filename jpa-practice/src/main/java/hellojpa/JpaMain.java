ff76a5ad768a0d7c9113d79479441c9aede136cd
sw-develop <psw00719@naver.com>
2022-03-30 23:56:30 +0900

    [JPA 기본] Add: Gradle, JPA, H2 설정 및 실습

---
+++ jpa-practice/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,36 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+  public static void main(String[] args){
+
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    EntityTransaction transaction = entityManager.getTransaction();
+    transaction.begin();
+
+    try{
+      Member member = new Member();
+      member.setId(1L);
+      member.setName("testA");
+
+      entityManager.persist(member);
+
+      transaction.commit();
+    } catch (Exception e) {
+
+      System.out.println(e.getMessage());
+
+      transaction.rollback();
+    } finally {
+      entityManager.close();
+    }
+
+    entityManagerFactory.close();
+  }
+}
