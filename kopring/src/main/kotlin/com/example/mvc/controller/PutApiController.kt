fade65474535e4091ee2235373075908108b5636
sw-develop <psw00719@naver.com>
2022-06-25 09:00:32 +0900

    Add: Bean Validation

--- kopring/src/main/kotlin/com/example/mvc/controller/PutApiController.kt
+++ kopring/src/main/kotlin/com/example/mvc/controller/PutApiController.kt
@@ -3,19 +3,37 @@ package com.example.mvc.controller
 import com.example.mvc.model.http.UserRequest
 import com.example.mvc.model.http.UserResponse
 import com.example.mvc.model.http.Result
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api")
 class PutApiController {
 
     @PutMapping("/put-mapping/object")
-    fun putMappingObject(@RequestBody userRequest: UserRequest): UserResponse {
+    fun putMappingObject(
+            @Valid @RequestBody userRequest: UserRequest, bindingResult: BindingResult
+    ): ResponseEntity<String> {
+        if (bindingResult.hasErrors()) {
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach {
+                val field = it as FieldError
+                val message = it.defaultMessage
+                msg.append(field.field + " : " + message + "\n")
+            }
+            return ResponseEntity.badRequest().body(msg.toString())
+        }
+
+        return ResponseEntity.ok("OK")
+
         //result, description, user mutable list
-        return UserResponse().apply {
+/*        return UserResponse().apply {
             this.result = Result().apply {
                 this.resultCode = "OK"
                 this.resultMessage = "성공"
@@ -26,6 +44,6 @@ class PutApiController {
             val userList = mutableListOf<UserRequest>()
             userList.add(userRequest)
             this.users = userList
-        }
+        }*/
     }
 }
--- kopring/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ kopring/src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,13 +1,37 @@
 package com.example.mvc.model.http
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.format.annotation.DateTimeFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.validation.constraints.*
 
 data class UserRequest(
+
+        @field:NotEmpty
+        @field:Size(min = 2, max = 8)
         var name: String? = null,
+
+        @field:PositiveOrZero
         var age: Int? = null,
+
+        @field:Email
         var email: String? = null,
+
+        @field:NotBlank
         var address: String? = null,
 
-        @JsonProperty("phone_number")
-        var phoneNumber: String? = null
-)
+        @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$")
+        var phoneNumber: String? = null,
+
+        var createdAt: String? = null //yyyy-MM-dd HH:mm:ss
+) {
+        @AssertTrue(message = "생성일자의 패턴은 yyyy-MM-dd HH:mm:ss 여야 합니다.")
+        private fun isValidCreatedAt(): Boolean {
+                return try {
+                        LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+                        true
+                } catch (e: java.lang.Exception) {
+                        false
+                }
+        }
+}
