b8b8da93a3873e896dbf6ff51d84aa4f5d2d92f6
sw-develop <psw00719@naver.com>
2022-06-25 10:04:48 +0900

    Add: ControllerAdvice

---
+++ kopring/src/main/kotlin/com/example/mvc/controller/ExceptionApiController.kt
@@ -0,0 +1,25 @@
+package com.example.mvc.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/exception")
+class ExceptionApiController {
+
+    @GetMapping("/hello")
+    fun hello() {
+        val list = mutableListOf<String>()
+        val temp = list[0]
+    }
+
+    @ExceptionHandler(value = [IndexOutOfBoundsException::class])
+    fun indexOutOfBoundsException(e: IndexOutOfBoundsException): ResponseEntity<String> {
+        println("ExceptionHandler에 의한 처리")
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Index Error")
+    }
+}
